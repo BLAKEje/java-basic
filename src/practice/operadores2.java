1be2a9d2523bb8c24d9442800899ba571c42a9f3
Alifer Rabelo <aliferrabelo2413@gmail.com>
2024-06-10 20:42:22 -0300

    upgrade

---
+++ src/practice/operadores2.java
@@ -0,0 +1,12 @@
+package practice;
+
+public class operadores2 {
+    public static void main(String[] args) {
+        int a, b;
+        a = 10;
+        b = 5;
+
+        String resultado = a == b ? "verdadeiro" : "falso";
+        System.out.println(resultado);
+    }
+}
---
+++ src/practice/operadores3.java
@@ -0,0 +1,14 @@
+package practice;
+
+public class operadores3 {
+    public static void main(String[] args) {
+        int numero1 = 10;
+        int numero2 = 5;
+
+        String verdadeiroFalso =  numero1 >= numero2 ? "verdadeiro" : "falso";
+        System.out.println(verdadeiroFalso);
+
+        // && = e
+        // | | = ou
+    }
+}
