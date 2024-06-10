495465782e65a08c85d468b096ce0d0579f35365
Alifer Rabelo <aliferrabelo2413@gmail.com>
2024-06-10 19:52:16 -0300

    upgrade

---
+++ src/practice/operadores1.java
@@ -0,0 +1,18 @@
+package practice;
+
+public class operadores1 {
+    public static void main(String[] args) {
+        int numero = 10;
+        ++ numero;
+        System.out.println(numero++);
+        System.out.println(numero--);
+        System.out.println(numero);
+
+        boolean variavel = true;
+        System.out.println(!variavel);
+        System.out.println(variavel);
+        variavel = !variavel;
+        System.out.println(variavel);
+
+    }
+}
--- src/practice/teste.java
+++ src/practice/teste.java
@@ -2,22 +2,9 @@ package practice;
 
 public class teste {
     public static void main(String[] args) {
+        String nomeCompleto = "Alifer" + " " + "Rabelo";
 
-        // os numeros usados no experimento
-        int numeroUm = 20;
-        int numeroDois = 15;
-
-        // puxando o metodo
-        String doisNumeros = doisNumeros (numeroUm, numeroDois);
-        System.out.println(doisNumeros);
-
-        // fazendo a soma
-        int soma = numeroUm + numeroDois;
-        System.out.println("A soma entre os dois números é: " + soma);
+        System.out.println(nomeCompleto);
 
     }
-    public static String doisNumeros (int numeroUm, int numeroDois) {
-        return "Os números utilizados no experimento foram: " + numeroUm + " e " + numeroDois;
-    };
-
 }
