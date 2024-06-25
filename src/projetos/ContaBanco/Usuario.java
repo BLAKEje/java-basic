7be2c5e3459836d5a124735ada7f3531862473c5
Alifer Rabelo <aliferrabelo2413@gmail.com>
2024-06-25 20:16:47 -0300

    upgrade

--- src/projetos/ContaBanco/Usuario.java
+++ src/projetos/ContaBanco/Usuario.java
@@ -7,7 +7,7 @@ public class Usuario{
         Scanner scanner = new Scanner(System.in);
         ContaTerminal contaTerminal = new ContaTerminal();
 
-        System.out.println("Qual o número da sua agência?");
+        System.out.println("Qual o número da sua agencia?");
         int numeroInteiro = scanner.nextInt();
         scanner.nextLine();
 
